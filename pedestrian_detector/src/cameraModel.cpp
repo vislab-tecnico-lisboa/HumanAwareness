3d399b93917ddbb24e1dd5dd57efabfbb7329d15
João Avelino <joao.manuel.avelino@gmail.com>
2015-05-30 18:21:02 +0100

    Frame change2. Seems good now. Now its time for some RViz tutorials...

--- pedestrian_detector/src/cameraModel.cpp
+++ pedestrian_detector/src/cameraModel.cpp
@@ -21,12 +21,12 @@ cameraModel::cameraModel(string configFile)
   invertedK.convertTo(invertedK, CV_32FC1);
 
   //Just initializing pose of the camera relative to the base of the robot...
-  float pose_array[] = {0, 1, 0, 0, 0, 0, -1, 500, -1, 0, 0, 0};
+  float pose_array[] = {0, 1, 0, 0, 0, 0, -1, 800, -1, 0, 0, 0};
   pose = Mat(3, 4, CV_32FC1, pose_array).clone();
 
   //If we consider the point to be on the floor, then relatively to the base frame they will have z=0
   //Therefore the 3rd column of the [R|t] matrix is 0, and we have and homography that we can invert.
-  float homography_array[] = {0, 1, 0, 0, 0, 500, -1, 0, 0};
+  float homography_array[] = {0, 1, 0, 0, 0, 800, -1, 0, 0};
   homography = Mat(3, 3, CV_32FC1, homography_array).clone();
 
 
